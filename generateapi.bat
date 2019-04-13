4a26bfa9724c5dc8cef83b06dd5b63d1c79b3a61
Marco Regueira <marcoregueira@gmail.com>
2019-04-13 18:28:03 +0200

    generateapi.bat

--- generateapi.bat
+++ generateapi.bat
@@ -1,3 +1,9 @@
+rd clients /s
+mkdir clients
+mkdir clients\swagger-ts-generator
+mkdir clients\swagger-typescript-codegen
+mkdir clients\swagger-angular-generator
+
 node generate.js > clients/swagger-typescript-codegen/client.js
 
 node generate2.js
